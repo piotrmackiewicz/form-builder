dd930c70ae88b249bf82ce4b88869097fdf0ef12
Piotr Mackiewicz <pmackiewiczlodz@gmail.com>
2020-04-23 20:12:38 +0200

    Added prettier file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React, { useState } from "react";
-import "App.scss";
-import "semantic-ui-css/semantic.min.css";
-import { Grid, Transition } from "semantic-ui-react";
-import FormPreview from "components/FormPreview/index";
-import ConfigurationPanel from "components/ConfigurationPanel/index";
-import styled from "styled-components";
+import React, { useState } from 'react'
+import 'App.scss'
+import 'semantic-ui-css/semantic.min.css'
+import { Grid, Transition } from 'semantic-ui-react'
+import FormPreview from 'components/FormPreview/index'
+import ConfigurationPanel from 'components/ConfigurationPanel/index'
+import styled from 'styled-components'
 
 const FormPreviewColumn = styled(Grid.Column)`
   background-color: red;
   z-index: 2;
-`;
+`
 
 const ConfigurationPanelColumn = styled(Grid.Column)`
   background-color: blue;
   z-index: 1;
-`;
+`
 
 function App() {
   const [configurationPanelVisible, setConfigurationPanelVisible] = useState(
     false
-  );
+  )
 
   const renderConfigurationPanel = () => (
     <ConfigurationPanelColumn
       width={8}
-      style={{ backgroundColor: "blue", zIndex: 1 }}
+      style={{ backgroundColor: 'blue', zIndex: 1 }}
     >
       <ConfigurationPanel />
     </ConfigurationPanelColumn>
-  );
+  )
 
   return (
     <Grid padded>
       <FormPreviewColumn
         width={8}
-        style={{ backgroundColor: "red", zIndex: 2 }}
+        style={{ backgroundColor: 'red', zIndex: 2 }}
       >
         <FormPreview
           onShowConfigPanelClick={() =>
@@ -46,7 +46,7 @@ function App() {
         {configurationPanelVisible && renderConfigurationPanel()}
       </Transition.Group>
     </Grid>
-  );
+  )
 }
 
-export default App;
+export default App
